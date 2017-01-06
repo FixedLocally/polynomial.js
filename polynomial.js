bbac5d9469e4fa99c0186ab98a5e5ab335df2227
lkp111138 <abc6a1@yahoo.com>
2017-01-06 11:49:37 +0000

    added poly_lip() and farey() to support fractions

--- polynomial.js
+++ polynomial.js
@@ -41,7 +41,7 @@ function poly_mul(l, r) {
         if ((l[0] == 0 && l.length == 1) || (r[0] == 0 && r.length == 1)) {
             return [0];
         }
-        console.log(l, r);
+        //console.log(l, r);
         var i;
         var q = JSON.parse(JSON.stringify(l));
         var t = [];
@@ -85,6 +85,9 @@ function poly_deg(p) {
     return i < 0 ? Number.NEGATIVE_INFINITY : i;
 }
 function poly_div(nu, de) {
+    if(de[0] == 0 && de.length == 0){ //zero ploynomial
+        throw "division by 0";
+    }
     if (poly_deg(nu) < poly_deg(de)) {
         return [0, nu];
     } else {
@@ -97,9 +100,9 @@ function poly_div(nu, de) {
         for (var i = degree; i >= 0; --i) {
             var x = poly_deg(nu) - degree + i;
             res[i] = n[x] / de[poly_deg(de)];
-            //console.log('div',0,i,x, res,de, n);
+            ////console.log('div',0,i,x, res,de, n);
             n = poly_add(nu, poly_mul(-1, res, de));
-            //console.log('div',1,i,x, res,de, n);
+            ////console.log('div',1,i,x, res,de, n);
         }
         return [res, n];
     }
@@ -110,4 +113,50 @@ function poly_differentiate(poly) {
     });
     a.shift();
     return a;
+}
+function poly_lip(points){
+    var tmp=[1];
+    var result=[0];
+    //var points=[[0, 0], [1, 1], [-2, -8], [5, 125], [10, 1000]];
+    for(i in points){
+        tmp=[1];
+        for(j in points){
+            if(i!=j){
+                console.log(i,j,tmp,[-points[j][0],1],points[i][0]-points[j][0]);
+                tmp=poly_mul(tmp,poly_div([-points[j][0],1],[points[i][0]-points[j][0]])[0]);
+            }
+        }
+        result=poly_add(poly_mul(tmp,points[i][1]),result);
+    }
+    return result;
+}
+function farey(x,N){
+    var a=0;
+    var b=1;
+    var c=1;
+    var d=1;
+    while(b<=N&&d<=N){
+        var mid=(a+c)/(b+d);
+        if(x==mid){
+            //console.log("%s == (%s+%s)/(%s+%s)",x,a,b,c,d);
+            if(b+d<=N) return [a+c,b+d];
+            if(d>b) return [c,d];
+            return [a,b]
+        }else{
+            if(x>mid){
+                //console.log("%s >  (%s+%s)/(%s+%s)",x,a,b,c,d);
+                a+=c;
+                b+=d;
+            }else{
+                //console.log("%s <= (%s+%s)/(%s+%s)",x,a,b,c,d); 
+                c+=a;
+                d+=b;
+            }
+        }
+    }
+    if(b>N){
+        return [c,d];
+    }else{
+        return [a,b];
+    }
 }
